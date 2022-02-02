2254a62659550274eec7926a45206e9dc32c8283
Francesca <fcantale14@gmail.com>
2022-02-02 22:13:16 +0100

    pop up

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,4 @@
-// login and redirect to index.html
-
-var login = document.querySelector('#login-form');
-var login_btn = document.querySelector('#login-btn');
-login_btn.addEventListener('click', function(e) {
-    e.preventDefault();
-    var email = login.querySelector('#email').value;
-    var password = login.querySelector('#password').value;
-    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
-        
-
-
+// pop up show when index.html is loaded
+$(document).ready(function() {
+    $('.login-popup').modal('show');
+});
