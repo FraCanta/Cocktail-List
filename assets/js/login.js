016460877347f43ab993bef0b9737ab4cf45d4cf
Francesca <fcantale14@gmail.com>
2022-02-21 12:19:02 +0100

    localstorage

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,26 +4,13 @@ const close = document.querySelector(".close");
 window.addEventListener("load", function () {
   const popUpWasShown= localStorage.getItem("popupWasShown");
   if (popUpWasShown == null) {
-  showPopup();
+    setTimeout(function(){
+    loginPopup.classList.add("show");
+  },1000)
+
   }
-  // setTimeout(function(){
-  //   loginPopup.classList.add("show");
-  // },5000)
 });
 
-function showPopup() {
- 
-  const timeLimit = 1; // seconds;
-  let i = 0;
-  const timer = setInterval(function () {
-    i++;
-    if (i == timeLimit) {
-      clearInterval(timer);
-      loginPopup.classList.add("show");
-    }
-    console.log(i);
-  }, 1000);
-}
 
 close.addEventListener("click", function () {
   loginPopup.classList.remove("show");
