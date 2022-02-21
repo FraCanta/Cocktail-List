d38f4e5d864595756f5bd1d2ccf1bbf5849ce506
Francesca <fcantale14@gmail.com>
2022-02-21 12:03:48 +0100

    localstorage

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,13 +2,17 @@ const loginPopup = document.querySelector(".login-popup");
 const close = document.querySelector(".close");
 
 window.addEventListener("load", function () {
+  const popUpWasShown= localStorage.getItem("popupWasShown");
+  if (popUpWasShown == null) {
   showPopup();
+  }
   // setTimeout(function(){
   //   loginPopup.classList.add("show");
   // },5000)
 });
 
 function showPopup() {
+ 
   const timeLimit = 1; // seconds;
   let i = 0;
   const timer = setInterval(function () {
@@ -33,9 +37,33 @@ $(document).ready(function () {
 // login se hai più o uguale ad anni 18
 
 function login() {
-  return loginPopup.remove("button-close");
+  localStorage.setItem("popupWasShown",1);
+  return (
+    loginPopup.remove("button-close")
+  );
+
 }
 
 function reject() {
-  alert("Non sei abbastanza vecchio per accedere a questa pagina");
+  confirm("Non sei abbastanza vecchio per accedere a questa pagina");
 }
+
+
+// function PopUp(hideOrshow) {
+//   if (hideOrshow == 'hide') {
+//       document.getElementById('ac-wrapper').style.display = "none";
+//   }
+//   else  if(localStorage.getItem("popupWasShown") == null) {
+//       localStorage.setItem("popupWasShown",1);
+//       document.getElementById('login-popup').removeAttribute('style');
+//   }
+// }
+// window.onload = function () {
+//   setTimeout(function () {
+//       PopUp('show');
+//   }, 0);
+// }
+
+
+// function hideNow(e) {
+//   if (e.target.id == 'login-popup') document.getElementById('login-popup').style.display = 'none';}
